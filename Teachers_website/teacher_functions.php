4fb81416dcd391b5370d1f0cc323ac7eef54de6d
phreesoft-kevin <kevin.premo@gmail.com>
2018-02-24 14:25:40 -0700

    Adding website plugins to git.

---
+++ Teachers_website/teacher_functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Teacher Functions
+ * @version 0.1
+ */
+/*
+Plugin Name: Teacher Functions
+Plugin URI:
+Description: This is a plugin for additonal features I want to add to wordpress.
+Author: Kevin Premo
+Version: 0.1
+Author URI:
+Text Domain: Teacher
+*/
+
+?>
